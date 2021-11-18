1bcbac50561c6f1848040d7a625cf521557b79cb
kichokhrizzz <jhosel.badillo.cortes@gmail.com>
2021-11-18 11:27:46 -0600

    18/11/2021 11:27 AM

---
+++ resources/views/recetas/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('botones')
+
+    <a href="{{ route('recetas.index') }}" class="btn btn-primary mr-2 text-white">Volver</a>
+
+@endsection
+
+@section('content')
+
+    <h2 class="text-center mb-5">Crear Nueva Receta</h2>
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form method="POST" action="{{ route('recetas.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="titulo">Titulo de la Receta</label>
+
+                    <input type="text" name="titulo" class="form-control" id="titulo" placeholder="Titulo de la receta" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta">
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
+@section('botones')
+
+    <a href="{{ route('recetas.create') }}" class="btn btn-primary mr-2 text-white">Crear Receta</a>
+
+@endsection
+
 @section('content')
+
+    <h2 class="text-center mb-5">Administra tus recetas</h2>
+
+    <div class="col-md-10 mx-auto bg-white p-3">
+        <table class="table">
+            <thead class="bg-primary text-light">
+                <tr>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 
 @endsection
